597cfee5e03b7da9cd3e9cf63babfd09f277be14
Bilal zafar <bilalafridi0020@gmail.com>
2023-08-26 11:44:18 +0500

    Props practice

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+function Hi(){
+
+return <div>
+
+  <h1>Hello </h1>
+
+
+
+
+</div>
+
+
+}
+
+
+ReactDOM.render(<Hi/>, document.querySelector('#root'));
